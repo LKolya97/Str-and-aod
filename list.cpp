43e26d568ea1050588b40750917b42758a7d3218
LKolya97 <LKolya97@gmail.com>
2016-12-05 12:12:42 +0300

    Add files via upload

--- list.cpp
+++ list.cpp
@@ -1,75 +1,48 @@
-#include 'list.h'
-#include 'player.h'
+// ConsoleApplication2.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
 #include <iostream>
+#include "player.h"
 using namespace std;
-template< Player T >
-List< T >::List() : m_head(NULL) {
-}
+struct element {
+	Player plr;
+	element *Next;
+};
 
-void List::printList{
-	р = first;
-while (p) {
-	cout « p->plr « » »;
-	р = p->next;
-}
-}
-template< Player T >
-void List< T >::append(const T &t) {
-	// Создаем новый узел для значения
-	// Не забудем проверить, что память удалось выделить
-	if (Node* node = new Node(t)) {
-		// Новый узел привязывается к старому головному элементу
-		node->m_next = m_head;
+class List {
+	element *Head;
+public:
+	List() { Head = NULL; }
+	~List();
+	void Add(Player plr);
+	void Show();
+};
 
-		// Новый узел сам становится головным элементом
-		m_head = node;
+void List::Add(Player plr) {
+	element *temp = new element;
+	temp->plr = plr;
+	temp->Next = Head;
+	Head = temp;
+}
+List::~List() {
+	while (Head != NULL) {
+		element *temp = Head->Next;
+		delete Head;
+		Head = temp;
 	}
 }
 
-template< Player T >
-void List< T >::remove() {
-	if (m_head) { // Если список не пуст:
-				  // Сохраняем указатель на второй узел, который станет новым головным элементом
-		Node* newHead = m_head->m_next;
-
-		// Освобождаем память, выделенную для удаляемого головного элемента
-		delete m_head;
-
-		// Назначаем новый головной элемент
-		m_head = newHead;
-	} // Иначе могли бы возбудить исключение
-}
-
-template< Player T >
-List< T >::~List() {
-	while (m_head) {
-		remove();
+void List::Show() {
+	element *temp = Head;
+	while (temp != NULL) {
+		cout << temp->plr << " ";
+			temp=temp->Next;
 	}
 }
-template< Player T >
-typename List< T >::Iterator List< T >::begin() const {
-	// Итератор пойдет от головного элемента...
-	return Iterator(m_head);
+int main()
+{
+
+    return 0;
 }
 
-template< Player T >
-typename List< T >::Iterator List< T >::end() const {
-	// ... и до упора, т.е. NULL
-	return Iterator(NULL);
-}
-template< Player T >
-size_t List< T >::size() const {
-	size_t s = 0;
-	for (Iterator it = begin(); it != end(); ++it) {
-		++s;
-	}
-
-	/*
-	Но можно и без итераторов
-	for( Node* n = m_head; n != NULL; n = n->m_next ) {
-	++s;
-	}
-	*/
-
-	return s;
-}
